941dedff514339753ea38b1a9b7cd1b5d700bea6
zarin01 <zarinneugebauer@gmail.com>
2024-10-17 21:28:32 -0600

    Adding email to register

--- Server/models/User.js
+++ Server/models/User.js
@@ -6,6 +6,11 @@ const UserSchema = new Schema({
     type: String,
     required: true,
     unique: true
+  }, 
+  email: {
+    type: String,
+    required: true,
+    unique: true
   },
   password: {
     type: String,
